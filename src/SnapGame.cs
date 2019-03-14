9b0f9f92ed8c9970925cd47afb8e808aace2b487
AnnaYong <nicoleboniyong@gmail.com>
2019-03-14 16:42:22 +0800

    TL changed it

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -17,17 +17,28 @@ namespace CardGames
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
+		private static void HandleUserInput (Snap myGame)
 		{
 			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
+			SwinGame.ProcessEvents ();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
-			{
+			if (SwinGame.KeyTyped (KeyCode.vk_SPACE)) {
 				myGame.Start ();
 			}
-		}
 
+			if (myGame.IsStarted) {
+				if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) &&
+				   SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
+					//TODO: add sounds effects
+				} else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) {
+					myGame.PlayerHit (0);
+				} else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
+					myGame.PlayerHit (1);
+				}
+
+
+			}
+		}
 		/// <summary>
 		/// Draws the game to the Window.
 		/// </summary>
