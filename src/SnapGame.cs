93c098c05a6fd7601413d9cb863c37eafe95aa3a
XavierNewer <WongKS07@hotmail.com>
2019-03-19 20:11:23 +0800

    Change made

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -12,7 +12,7 @@ namespace CardGames
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
             SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
 			SwinGame.LoadFontNamed ("GameFont","Chunkfive.otf", 24);
-			SwinGame.LoadSoundEffectNamed ("Slap", "Slap.wav");
+			SwinGame.LoadSoundEffectNamed ("Slap", "Slap.mp3");
         }
 
 		/// <summary>
